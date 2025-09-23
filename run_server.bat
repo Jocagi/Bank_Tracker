3b9d5c5bf1fa5a19d6ac78778476e72c91b27739
José Girón <42883411+Jocagi@users.noreply.github.com>
2025-09-23 16:35:11 -0600

    feat: Update run_server.bat to activate virtual environment before starting Waitress server

--- run_server.bat
+++ run_server.bat
@@ -1,3 +1,5 @@
 @echo off
 REM -- Iniciar servidor con Waitress en el puerto 5600
-waitress-serve --host=0.0.0.0 --port=5600 main:app
+REM -- Activar el entorno virtual y usar waitress desde allí
+call .venv\Scripts\activate.bat
+.venv\Scripts\waitress-serve --host=0.0.0.0 --port=5600 main:app
